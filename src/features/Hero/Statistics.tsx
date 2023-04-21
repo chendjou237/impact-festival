5568e7a65a84ed3ee0322be67cad031236e1c44e
chendjou237 <chenxhenor@gmail.com>
2023-04-21 12:46:01 +0100

    update sur le table

--- src/features/Hero/Statistics.tsx
+++ src/features/Hero/Statistics.tsx
@@ -19,7 +19,7 @@ const Table = () => {
         <h2 className='text-white font-bold
          text-lg font-cinzel'>Confirmed</h2>
         <div className=' bg-diamond  px-2 py-2 md:py-4   rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  flex items-c
-        enter '><div className="rounded-full font-biko  mr-2 p-1 self-center bg-white"><ReactSVG src={diamond} /></div> <span className="hidden md:block font-cinzel">DIAMOND</span></div>
+        enter '><div className="rounded-full font-biko  mr-2 p-1 self-center bg-black"><ReactSVG src={diamond} /></div> <span className="hidden md:block font-cinzel">DIAMOND</span></div>
         <div className=' bg-diamond  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 12</div>
         
         <div className=' bg-diamond  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 40</div>
@@ -28,7 +28,7 @@ const Table = () => {
 
         <div className=' bg-diamond  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 0</div>
         <div className=' bg-gold  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg text-white w-20 md:justify-start justify-center md:w-40 flex items-c
-        enter '><div className="rounded-full font-biko mr-2 p-1 self-center bg-white"><ReactSVG src={gold} /></div> <span className="hidden md:block font-cinzel">GOLD</span></div>
+        enter '><div className="rounded-full font-biko mr-2 p-1 self-center bg-white "><ReactSVG src={gold}  /></div> <span className="hidden md:block font-cinzel">GOLD</span></div>
         <div className=' bg-gold  px-2 py-2 md:py-4 rounded-md font-biko text-white font-semibold  text-lg w-20 md:justify-start justify-center md:w-40  '> 23</div>
         
         <div className=' bg-gold  px-2 py-2 md:py-4 rounded-md font-biko text-white font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 50</div>
@@ -37,7 +37,7 @@ const Table = () => {
 
         <div className=' bg-gold  px-2 py-2 md:py-4 rounded-md font-biko text-white font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 0</div>
         <div className=' bg-silver  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20  md:w-40 flex md:justify-start justify-center  items-c
-        enter  '><div className="rounded-full font-biko mr-2  p-1 self-center bg-white"><ReactSVG src={silver} /></div>  <span className="hidden md:block font-cinzel">SILVER</span></div>
+        enter  '><div className="rounded-full font-biko mr-2  p-1 self-center bg-black"><ReactSVG src={silver} /></div>  <span className="hidden md:block font-cinzel">SILVER</span></div>
         <div className=' bg-silver  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 30</div>
         
         <div className=' bg-silver  px-2 py-2 md:py-4 rounded-md font-biko  font-semibold text-lg w-20 md:justify-start justify-center md:w-40  '> 80</div>
